e88c411b9902e6545ea06e44194a84be77a05d29
RCD2020 <dobertravis@icloud.com>
2021-09-20 11:37:20 -0600

    added rico code

--- spotty/app.py
+++ spotty/app.py
@@ -1,6 +1,6 @@
 """Web application functions
 
-Robert Davis
+Rico & Robert
 2021/09/20"""
 
 
@@ -8,10 +8,22 @@ from flask import Flask, render_template, request
 
 
 def create_app():
-    """Creates the application"""
+    """Create App"""
 
     app = Flask(__name__)
 
+    
+    @app.route('/')
+    def base():
+        """Creates the home page"""
+
+        return 'Spotify API'
+
+    @app.route('/song_suggestor')
+    def song_suggestor():
+        """Create a suggestor route"""
+
+        return 'song suggestions'
 
 
     return app
